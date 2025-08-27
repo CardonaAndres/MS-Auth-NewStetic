af95291f43a18411e24f5a3a88c5d58f9165f364
PTIC2 <ptic2@newstetic.com>
2025-08-27 12:25:21 -0500

    feat: mejorar manejo de errores en la autenticación de usuarios

--- src/auth/controllers/auth.controller.js
+++ src/auth/controllers/auth.controller.js
@@ -5,7 +5,6 @@ import { AllowedUsers } from '../utils/defineAllowedUsers.js';
 export class AuthController {
     static async login(req, res, next) {
         try {
-
             const { username, password, appName } = req.body;
             const usernamesAllowed = AllowedUsers.defineAllowedUsers(appName);
 
@@ -30,60 +29,59 @@ export class AuthController {
 
             const ad = new ActiveDirectory(userConfig);
 
-            ad.authenticate(userConfig.username, password, (err, auth) => {
-                if (err) {
-                    return res.status(500).json({
-                        message: '❌ Usuario bloqueado. Comuníquese con la mesa de ayuda.'
-                    });
-                }
+                ad.authenticate(userConfig.username, password, (err, auth) => {
+                    if (err) {
+                        let msg = '❌ Error de autenticación. Intente nuevamente o comuníquese con la mesa de ayuda.';
+                        if (err.message) {
+                            if (err.message.includes('775')) 
+                                msg = '❌ Usuario bloqueado. Comuníquese con la mesa de ayuda.'
 
-                if(!auth) return res.status(401).json({ message: '❌ Usuario o contraseña incorrectos' })
-                
-                // Buscar usuario y obtener atributos específicos
-                const attributes = [
-                    'displayName', 'badPwdCount', 'lockoutTime', 'department',
-                    'cn', 'sn', 'givenName', 'mail', 'telephoneNumber'
-                ];
+                            if (err.message.includes('52e')) msg = '❌ Usuario o contraseña incorrectos';
+                            
+                            if (err.message.includes('532')) msg = '❌ La contraseña ha expirado. Debe cambiarla.';  
+                        }
 
-                ad.findUser(username, attributes, (err, user) => {
-                    if (err || !user) 
-                        return res.status(401).json({ message: '❌ Usuario o contraseña incorrectos' });
+                        return res.status(401).json({ message: msg });
+                    }
+
+                    if(!auth) return res.status(401).json({ message: '❌ Usuario o contraseña incorrectos' })
+
+                    ad.findUser({ attributes: ["*"] }, username, (err, user) => {
+                        if (err || !user) 
+                            return res.status(401).json({ message: '❌ Usuario o contraseña incorrectos' });
                     
-                    const userInfo = {
-                        displayName: user.displayName || username,
-                        username: username,
-                        department: user.department,
-                        cn: user.cn || '',
-                        sn: user.sn || '',
-                        givenName: user.givenName || '',
-                        mail: user.mail || '',
-                        telephoneNumber: user.telephoneNumber || ''
-                    };
+                        const userInfo = {
+                            displayName: user.displayName || username,
+                            username: username,
+                            department: user.department,
+                            cn: user.cn || '',
+                            sn: user.sn || '',
+                            givenName: user.givenName || '',
+                            mail: user.userPrincipalName || '',
+                        };
 
-                    JWT.createTokenAccess(userInfo).then(token => {
-                        res.cookie('token', token, {
-                            maxAge: 1000 * 60 * 60,
-                           // signed: true 
-                        });
+                        JWT.createTokenAccess(userInfo).then(token => {
+                            res.cookie('token', token, {
+                                maxAge: 1000 * 60 * 60,
+                            });
 
-                        return res.status(200).json({
-                            message: '✅ Usuario autenticado correctamente',
-                            user : {
-                                ...userInfo,
-                                token
-                            }
-                        });
+                            return res.status(200).json({
+                                message: '✅ Usuario autenticado correctamente',
+                                user : {
+                                    ...userInfo,
+                                    token
+                                }
+                            });
                         
-                    }).catch(err => {
-                        return res.status(500).json({
-                            message: '❌ Error al generar el token de acceso',
-                            error: err.message
+                        }).catch(err => {
+                            return res.status(500).json({
+                                message: '❌ Error al generar el token de acceso',
+                                error: err.message
+                            });
                         });
-                    });
 
+                    });
                 });
-                
-            });
 
         } catch (err) {
             next(err);
